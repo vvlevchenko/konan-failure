8ff93ee6dbb0d82c0db309dbff76d2f7bebfac6b
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2018-07-04 14:54:43 +0300

    initial commit

---
+++ src/main/kotlin/simple.kt
@@ -0,0 +1,9 @@
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.CoroutineContext
+
+internal open class Test(
+    parentContext: CoroutineContext
+) : AbstractCoroutine<Unit>(parentContext, active = true)
+
+
+fun main(args:Array<String>) {}
